5fcb1efb2047f27b50fea0bc73baa983d904f236
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-25 23:12:00 +0100

    Update Footer

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="bg-black">
-        <p class="text-white">Ce site a été réalisé avec Nuxt et hébérgé sur Heroku.<br/>L’api à été créé avec Strapi et ne collecte aucune donnée.<br/>Vous pouvez trouver le code source sur Github. </p>
+        <div class="py-20 flex justify-center">
+            <p class="text-white">Ce site a été réalisé avec Nuxt et hébérgé sur Heroku.<br/>L’api à été créé avec Strapi et ne collecte aucune donnée.<br/>Vous pouvez trouver le code source sur <a class="text-purple" href="https://github.com/Cosme711/portfolio" target="_blank">Github</a>.</p>
+        </div>
+        <div class="pb-10 py-1 w-8/12 m-auto flex justify-between text-gray-DEFAULT border-t-2 border-gray-DEFAULT">
+            <p>© 2021 - Cosme Gressier</p>
+            <p>Mentions légales</p>
+        </div>
     </div>
 </template>
 
