e759186d79fa01425f7e978f7b40c084b4012f9c
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-28 22:24:06 +0100

    Google lighthouse optimization

--- components/Slider/Slider.vue
+++ components/Slider/Slider.vue
@@ -8,8 +8,8 @@
             <img src="~/assets/icons/Arrow_Logo.svg" class="cursor-pointer" alt="Arrow Logo" width="23" height="23">
         </div>
     </div>
-    <Hooper style="height: auto" class="outline-none" ref="slider" :settings="hooperSettings">
-        <Slide v-for="slide in sliderData" :key="slide.id" aria-hidden="true" tabindex="-1">
+    <Hooper style="height: auto" class="outline-none" ref="slider" aria-hidden="true" :settings="hooperSettings">
+        <Slide v-for="slide in sliderData" :key="slide.id" tabindex="-1">
             <a :href="slide.link" @click="clickSlide" target="_blank" rel="noreferrer">
                 <img 
                     :src="slide.screenshot[0].url" 
