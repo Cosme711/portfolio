528a5aa0a57ac7ce605638b360d5eae74b726dc6
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-02-28 22:33:23 +0100

    Google lighthouse optimization

--- components/Slider/Slider.vue
+++ components/Slider/Slider.vue
@@ -9,8 +9,8 @@
         </div>
     </div>
     <Hooper style="height: auto" class="outline-none" ref="slider" aria-hidden="true" tabindex="-1" :settings="hooperSettings">
-        <Slide v-for="slide in sliderData" :key="slide.id" tabindex="-1">
-            <a :href="slide.link" @click="clickSlide" target="_blank" rel="noreferrer">
+        <Slide v-for="slide in sliderData" :key="slide.id" aria-hidden="true" tabindex="-1">
+            <a :href="slide.link" @click="clickSlide" aria-hidden="true" target="_blank" rel="noreferrer">
                 <img 
                     :src="slide.screenshot[0].url" 
                     :alt="slide.alt" 
