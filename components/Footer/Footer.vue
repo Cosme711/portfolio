b1fa7c06cba7ff2fa64c85220a04f89fd40505cc
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-03-08 21:39:49 +0100

    Footer correction

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-black">
         <div class="py-20 w-10/12 m-auto 5:w-auto flex justify-center">
-            <p class="text-white">Ce site a été réalisé avec Nuxt et hébérgé sur Heroku.<br/>L’api à été créé avec Strapi et ne collecte aucune donnée.<br/>Vous pouvez trouver le code source sur <a class="underline" href="https://github.com/Cosme711/portfolio" target="_blank" rel="noreferrer">Github</a>.</p>
+            <p class="text-white">Ce site a été réalisé avec Nuxt et hébergé sur Heroku.<br/>L’api à été créé avec Strapi et ne collecte aucune donnée.<br/>Vous pouvez trouver le code source sur <a class="underline" href="https://github.com/Cosme711/portfolio" target="_blank" rel="noreferrer">Github</a>.</p>
         </div>
         <div class="pb-10 py-1 w-10/12 6:w-8/12 m-auto flex text-center 4:justify-between flex-col 4:flex-row text-white border-t-2 border-gray-midlle">
             <p>Mentions légales</p>
