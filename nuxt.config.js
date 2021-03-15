f7ab5c9e5b70b6f85f653d3e6af8f1b63f0934ae
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-03-15 10:19:34 +0100

    Clean Code

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,15 +18,6 @@ export default {
 
   components: true,
 
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
@@ -54,13 +45,5 @@ export default {
 
   googleAnalytics: {
     id: 'UA-183930246-1'
-  },
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
   }
 }
