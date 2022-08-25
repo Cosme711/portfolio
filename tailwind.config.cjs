b3a68fed061d7ff9111c078acde5c7cf8b726285
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2022-08-25 08:43:26 +0200

    feat: add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      ubuntu: 'Ubuntu',
+      roboto: 'Roboto',
+    },
     extend: {},
   },
   plugins: [],
